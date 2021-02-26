83e4a838095826924b116214bca95549c625e386
Gaurav Gandhi <imgg.gauravgandhi@gmail.com>
2021-02-26 18:15:06 +0530

    Update main app to pint blog app

--- my_first_django_project/urls.py
+++ my_first_django_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from blog import urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('blog/', include('blog.urls'))
+    path('', include(urls))
 ]
-
